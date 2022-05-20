ef2a7425e3b861af7ba2041a5f4cdb3161ddeafb
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2022-05-20 15:10:03 -0500

    conexion
    archivo de conexion a la BD

---
+++ DAL/conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+class conexion
+{
+    static public function conectar()
+    {
+        $contrasena = "";
+        $usuario = "root";
+        $BaseDeDatos = "albergue_dogs";
+        $servidor = "localhost";
+        try {
+            $conexion = new PDO("mysql:host=$servidor;  dbname=$BaseDeDatos", $usuario, $contrasena);
+            $conexion->setAttribute(PDO::ATTR_ERRMODE,  PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            echo "Ocurrió un ERROR con la base de datos: " .    $e->getMessage();
+        }
+        return $conexion;
+    }
+}
+?>
