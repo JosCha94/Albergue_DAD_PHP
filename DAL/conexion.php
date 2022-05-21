a3da13e73db242d1b8d7b8f6dfc51e7c1f3a7672
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2022-05-20 21:33:14 -0500

    bd

--- DAL/conexion.php
+++ DAL/conexion.php
@@ -6,7 +6,7 @@ class conexion
     {
         $contrasena = "";
         $usuario = "root";
-        $BaseDeDatos = "albergue_dogs_prueba";
+        $BaseDeDatos = "albergue_dogs";
         $servidor = "localhost";
         try {
             $conexion = new PDO("mysql:host=$servidor;  dbname=$BaseDeDatos", $usuario, $contrasena);
