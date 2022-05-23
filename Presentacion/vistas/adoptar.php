7cdd4c7eaab029f848bbc6d5b6502721c7f10686
FabricioRMK3 <fabriciovc16@gmail.com>
2022-05-23 14:22:30 -0500

    Ya se muestran las imagenes dinamicamente en Adoptar

--- Presentacion/vistas/adoptar.php
+++ Presentacion/vistas/adoptar.php
@@ -1,13 +1,11 @@
 <?php
 require_once('BL/consultas_adopcion.php');
 require_once('DAL/conexion.php');
-require_once('DAL/clases/perritos.php');
 require_once('DAL/clases/img_perritos.php');
 
 $conexion = conexion::conectar();
 $consulta = new Consulta_perro();
 
-$perro = $consulta->listarPerro($conexion);
 $imgPerro = $consulta->listarImagen_perro($conexion);
 
 ?>
@@ -107,43 +105,17 @@ $imgPerro = $consulta->listarImagen_perro($conexion);
         <div class="col-md-9">
             <!-- perritos -->
             <div class="wrapper-gallery row magnific-popup mt-5">
-            <?php foreach ($perro as $key => $value) : ?>
-                
-               <!-- Image 1 -->
+                <?php foreach ($imgPerro as $key => $value) : ?>
                <div class="item-gallery col-lg-4 col-md-6 mb-5">
-                   
                   <div class="polaroid-gallery">
                      <a href="index.php?modulo=adoptar-single">
-                     <?php foreach ($imgPerro as $keyImg => $valueImg) : ?>
-                        <img width="" src="data:image/jpeg;base64,<?php echo base64_encode( $valueImg ); ?>" alt="" class="img-fluid">
-                     <?php endforeach; ?>
+                        <img src="data:image/<?php echo($value['img_perro_tipo']);?>;base64,<?php echo base64_encode( $value['img_perro_foto'] ); ?>" alt="" class="img-fluid">
                         <p class="caption-gallery" data-aos="zoom-in"><?=$value['perro_nombre'];?></p>
                      </a>
                   </div>
                </div>
             <?php endforeach; ?>
             </div>   
-            <div class="row sorting mb-5 mt-5">
-                <div class="col-8">
-                    <a class="btn btn-light"><i class="fas fa-arrow-up mr-2"></i> Back to top</a>
-                </div>
-                <div class="col-4">  
-                    <div class="dropdown float-md-right">
-                        <label class="mr-2">View:</label>
-                        <a class="btn btn-light btn-lg dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">12 <span class="caret"></span></a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">12</a>
-                            <a class="dropdown-item" href="#">24</a>
-                            <a class="dropdown-item" href="#">48</a>
-                            <a class="dropdown-item" href="#">96</a>
-                        </div>
-                        <div class="btn-group float-md-right ml-3">
-                            <button type="button" class="btn btn-lg btn-light"> <span class="fa fa-arrow-left"></span> </button>
-                            <button type="button" class="btn btn-lg btn-light"> <span class="fa fa-arrow-right"></span> </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </div>
