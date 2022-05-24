66aaa296f37904eb56d4eda6f03a2c61ee0808a2
FZafiro <f.humpiri.pari@isur.edu.pe>
2022-05-24 09:29:44 -0500

    vista de blog

--- Presentacion/vistas/blog.php
+++ Presentacion/vistas/blog.php
@@ -1,1 +1,101 @@
-<h1 class="text-center"> VISTA DE BLOG</h1>
+ <!-- ==== Page Content ==== -->
+ <div id="blog-home" class="page mt-5">
+         <div class="container">
+            <div class="row">
+               <!-- Blog Entries Column -->
+               <div class="col-lg-8 page-with-sidebar">
+                  <!-- Blog Post -->
+                  <div class="card blog-card ">
+                     <!--  Post info -->
+                     <div class="post-info mt-3">
+                        <ul class="list-inline m-0 p-0">
+                           <li class="list-inline-item"> 21 de Febrero del 2022</li>
+                           <li class="list-inline-item"> Por <a href="#">Autor</a></li>
+                        </ul>
+                     </div>
+                     <!-- /Post-info -->
+                     <a href="index.php?modulo=blog-single">
+                        <!-- image -->
+                        <div class="blog-img">
+                           <img class="card-img-top img-fluid" src="Presentacion/libs/images/blog1.jpg" alt="albergue">
+                        </div>
+                        <!-- /blog-img -->
+                     </a>
+                     <div class="card-body">
+                        <a href="blog-single.html">
+                           <h3 class="card-title">10 señales de un barrio que admite perros</h3>
+                        </a>
+                        <!-- excerpt -->
+                        <p class="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                        <a href="index.php?modulo=blog-single" class="btn btn-secondary">Leer más &rarr;</a>
+                     </div>
+                     <!--card-footer -->
+                  </div>
+
+                  <div class="card blog-card mb-3">
+                     <!--  Post info -->
+                     <div class="post-info mt-3">
+                        <ul class="list-inline m-0 p-0">
+                           <li class="list-inline-item"> 21 de Febrero del 2022</li>
+                           <li class="list-inline-item"> Por <a href="#">Autor</a></li>
+                        </ul>
+                     </div>
+                     <!-- /Post-info -->
+                     <a href="index.php?modulo=blog-single">
+                        <!-- image -->
+                        <div class="blog-img">
+                           <img class="card-img-top img-fluid" src="Presentacion/libs/images/blog1.jpg" alt="albergue">
+                        </div>
+                        <!-- /blog-img -->
+                     </a>
+                     <div class="card-body">
+                        <a href="blog-single.html">
+                           <h3 class="card-title">10 señales de un barrio que admite perros</h3>
+                        </a>
+                        <!-- excerpt -->
+                        <p class="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                        <a href="index.php?modulo=blog-single" class="btn btn-secondary">Leer más &rarr;</a>
+                     </div>
+                     <!--card-footer -->
+                  </div>
+                 
+               </div>
+               <!-- /page-with-sidebar -->
+               <!-- Sidebar -->
+               <div id="sidebar" class="h-100 col-lg-4 card">
+                  <!--widget-area -->
+                  <div class="widget-area">
+                     <h5 class="sidebar-header">Buscar</h5>
+                     <div class="input-group">
+                        <input type="text" class="form-control border border-secondary" placeholder="">
+                        <span class="input-group-btn">
+                        <button class="btn btn-secondary btn-sm" type="button">Ir</button>
+                        </span>
+                     </div>
+                  </div>
+                 
+               
+                  <!--/widget-area -->
+                  <div class="widget-area mt-3">
+                     <h5 class="sidebar-header">Síguenos</h5>
+                     <div class="contact-icon-info">
+                        <!-- Start Social Links -->
+                        <ul class="social-list text-center list-inline">
+                           <li class="list-inline-item"><a title="Facebook" href="#"><i class="fab fa-facebook-f"></i></a></li>
+                           <li class="list-inline-item"><a title="Twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                           <li class="list-inline-item"><a  title="Instagram" href="#"><i class="fab fa-instagram"></i></a></li>
+                          
+                        </ul>
+                        <!-- /End Social Links -->
+                     </div>
+                     <!--/contact-icon-info -->
+                  </div>
+                  <!--/widget-area -->
+               </div>
+               <!--/sidebar -->      
+            </div>
+            <!-- /.row -->
+         </div>
+         <!-- /.container -->
+      </div>
+      <!-- /page -->
