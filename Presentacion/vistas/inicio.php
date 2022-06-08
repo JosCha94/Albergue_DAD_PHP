72cb1e99366a02a3d789c150ea9b40045c3492b2
FZafiro <f.humpiri.pari@isur.edu.pe>
2022-06-07 20:33:07 -0500

    mision, vision de pagina de inicio

--- Presentacion/vistas/inicio.php
+++ Presentacion/vistas/inicio.php
@@ -1,10 +1,48 @@
 <div class="row d-flex justify-content-center">
-    <img class="img-fluid w-50" src="Presentacion/libs/images/PAGINA.jpg" alt="construccion">
+   <img class="img-fluid w-50" src="Presentacion/libs/images/PAGINA.jpg" alt="construccion">
 </div>
 <div class="row d-flex justify-content-center">
-    <div class="lds-facebook">
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
+   <div class="lds-facebook">
+      <div></div>
+      <div></div>
+   <div></div>
 </div>
+</div>
+<!-- section -->
+<section  class="container-fluid">
+         <!-- container -->  
+         <div class="container page-scroll negative-margin-200 ">
+            <div class="row mb-3">
+               <div class="col-lg-4 icon-box text-center">
+                  <div class="icon-wrapper bg-icon" >
+                     <!-- icon -->
+                     <i class="flaticon-dog-puppy-sitting-in-front-of-his-man"></i>
+                  </div>
+                  <h4> Nuestra visión</h4>
+                  <p>Una comunidad en la que todos los perritos viven en hogares seguros y cariñosos.</p>                  	                  
+               </div>
+               <!-- /col-lg-->
+               <div class="col-lg-4 icon-box text-center">
+                  <div class="icon-wrapper bg-icon">
+                     <!-- icon -->
+                     <i class="flaticon-man-on-his-knees-to-cuddle-his-dog"></i>
+                  </div>
+                  <h4>Nuestra misión</h4>
+                  <p>Nuestro albergue se dedica a cuidar y realojar a los perritos perdidos y no deseados.</p>                 
+               </div>
+               <!-- /col-lg-->
+               <div class="col-lg-4 icon-box text-center">
+                  <div class="icon-wrapper bg-icon">
+                     <!-- icon -->
+                     <i class="flaticon-running-dog-silhouette"></i>
+                  </div>
+                  <h4>Nuestro principio</h4>
+                  <p>Los perritos del albergue tienen derecho al mismo amor y respeto que ellos nos dan incondicionalmente.</p>                 
+               </div>
+               <!-- /col-lg-->      
+            </div>
+            <!-- /row -->
+         </div>
+         <!-- /container -->
+      </section>
+      <!-- /section ends -->
