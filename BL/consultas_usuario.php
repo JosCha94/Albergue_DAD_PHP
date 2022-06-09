ada779771a3a26212619e46fc3a7a7f16cc30160
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2022-06-09 00:32:17 -0500

    funcion update password

--- BL/consultas_usuario.php
+++ BL/consultas_usuario.php
@@ -157,8 +157,46 @@ class Consulta_usuario
             };
             $estado='mal';
         }
+        return $estado;        
+    }
+
+    public function Validar_pass($pass1, $pass2)
+    {
+        $errores = [];
+        $pass1 = trim($pass1);
+        $pass2 = trim($pass2);
+        if (empty($pass1)) {
+            $errores['pass1'] = "El campo Contraseña es requerido";
+        } elseif (strlen($pass1) < 8 || strlen($pass1) > 20) {
+            $errores['pass1'] = "La contraseña debe tener de 8 a 20 caracteres, sin espacios en blanco";        }
+        if (empty($pass2)) {
+            $errores['pass2'] = "El campo Repetir Contraseña es requerido";
+        } elseif ($pass1 != $pass2) {
+            $errores['pass2'] = "Las contraseñas no coinciden";
+        }
+        return $errores;
+    }
+    
+    public function cambiar_pass($conexion, $uid, $pass)
+    {
+        try {
+            $sql = "CALL SP_update_clave_usuario($uid, $pass)";
+            $consulta = $conexion->prepare($sql);
+            $consulta->execute();
+            $estado='bien';
+
+        } catch (PDOException $e) {
+            // echo "Ocurrió un ERROR con la base de datos: " .    $e->getMessage();
+            ?>
+              <div class="alert alert-danger alert-dismissible fade show " role="alert">
+              <strong>Error!</strong> Devido a un error en la base de datos, no se pudo actualizar la contraseña
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+            <?php
+            $estado='mal';
+        }
         return $estado;
-        
     }
 }
 ?>
--- Presentacion/vistas/update-usuario.php
+++ Presentacion/vistas/update-usuario.php
@@ -8,8 +8,8 @@ switch ($error = 'SinError') {
         break;
 }
 ?>
-<?php if ($error == 'SinError'): ?>
-<?php
+<?php if ($error == 'SinError') : ?>
+    <?php
     require_once('BL/consultas_usuario.php');
     require_once 'ENTIDADES/usuario.php';
     require_once('DAL/conexion.php');
@@ -31,7 +31,7 @@ switch ($error = 'SinError') {
         $consulta = new Consulta_usuario();
         $errores = $consulta->Validar_registro($usu);
         if (count($errores) == 0) {
-              $estado = $consulta->actualizar_usuario($conexion, $id, $usu);
+            $estado = $consulta->actualizar_usuario($conexion, $id, $usu);
 
             if ($estado == 'mal') {
             } else {
@@ -39,8 +39,22 @@ switch ($error = 'SinError') {
             }
         }
     }
-?>
-<?php if ($formTipo == 'dataUser') : ?>
+
+    if (isset($_POST['cambiar_pass'])) {
+        $pass1 = $_POST['pass'];
+        $pass2 = $_POST['pass2'];
+        $errores = $consulta->Validar_pass($pass1, $pass2);
+        if (count($errores) == 0) {
+            $estado = $consulta->cambiar_pass($conexion, $id, $pass1);
+
+            if ($estado == 'mal') {
+            } else {
+                echo '<meta http-equiv="refresh" content="0; url=index.php?modulo=update-user&formTipo=passUser&mensaje=La contraseña se cambio correctamente" />';
+            }
+        }
+    }
+    ?>
+    <?php if ($formTipo == 'dataUser') : ?>
         <div class="col-12 col-md-8 border border-dark p-5 mx-auto my-4 bg-primary" id="formData">
             <h3>INGRESA TUS DATOS PARA CAMBIAR</h3>
             <form action="" method="POST">
@@ -57,41 +71,71 @@ switch ($error = 'SinError') {
                 <div class="form-group">
                     <label for="usr_nombre">NICK DE USUARIO</label>
                     <input type="text" class="form-control" id="nick_user" name="nick_user" maxlength="15" minlength="5" value="<?php if (isset($user)) {
-                    echo $user;} else {echo ($usuario['usuario']);} ?>">
+                                                                                                                                    echo $user;
+                                                                                                                                } else {
+                                                                                                                                    echo ($usuario['usuario']);
+                                                                                                                                } ?>">
                 </div>
                 <div class="form-group">
                     <label for="usr_nombre">Nombre</label>
                     <input type="text" class="form-control" id="usr_nombre" name="usr_nombre" minlength="4" maxlength="20" value="<?php if (isset($name)) {
-                    echo $name;} else {echo ($usuario['usr_nombre']); } ?>">
+                                                                                                                                        echo $name;
+                                                                                                                                    } else {
+                                                                                                                                        echo ($usuario['usr_nombre']);
+                                                                                                                                    } ?>">
                 </div>
                 <div class="form-group">
                     <label for="usr_apellido_paterno">Apellido Paterno</label>
-                    <input type="text" class="form-control" id="usr_apellido_paterno" name="usr_apellido_paterno" minlength="4" maxlength="20" value="<?php if (isset($ape_p)) {echo $ape_p;} else {echo ($usuario['usr_apellido_paterno']);} ?>">
+                    <input type="text" class="form-control" id="usr_apellido_paterno" name="usr_apellido_paterno" minlength="4" maxlength="20" value="<?php if (isset($ape_p)) {
+                                                                                                                                                            echo $ape_p;
+                                                                                                                                                        } else {
+                                                                                                                                                            echo ($usuario['usr_apellido_paterno']);
+                                                                                                                                                        } ?>">
                 </div>
                 <div class="form-group">
                     <label for="usr_apellido_materno">Apellido Materno</label>
-                    <input type="text" class="form-control" id="usr_apellido_materno" name="usr_apellido_materno" minlength="4" maxlength="20" value="<?php if (isset($ape_m)) {echo $ape_m;} else {echo ($usuario['usr_apellido_materno']);} ?>">
+                    <input type="text" class="form-control" id="usr_apellido_materno" name="usr_apellido_materno" minlength="4" maxlength="20" value="<?php if (isset($ape_m)) {
+                                                                                                                                                            echo $ape_m;
+                                                                                                                                                        } else {
+                                                                                                                                                            echo ($usuario['usr_apellido_materno']);
+                                                                                                                                                        } ?>">
                 </div>
                 <div class="form-group">
                     <label for="usr_email">E-mail</label>
                     <input type="text" class="form-control" id="usr_email" name="usr_email" maxlength="30" value="<?php if (isset($email)) {
-                    echo $email;} else {echo ($usuario['usr_email']);} ?>">
+                                                                                                                        echo $email;
+                                                                                                                    } else {
+                                                                                                                        echo ($usuario['usr_email']);
+                                                                                                                    } ?>">
                 </div>
                 <div class="form-group">
                     <label for="usr_celular">Celular</label>
                     <input type="text" class="form-control" id="usr_celular" name="usr_celular" maxlength="9" minlength="9" value="<?php if (isset($celu)) {
-                    echo $celu;} else {echo ($usuario['usr_celular']);} ?>">
+                                                                                                                                        echo $celu;
+                                                                                                                                    } else {
+                                                                                                                                        echo ($usuario['usr_celular']);
+                                                                                                                                    } ?>">
                 </div>
-                
+
                 <button class="btn btn-primary my-3" name="actualizar_data">Actualizar</button>
                 <button type="reset" class="btn btn-danger my-3 mx-3" id="btn-cleanFormData">Limpiar</button>
             </form>
         </div>
 
     <?php elseif ($formTipo == 'passUser') : ?>
-        <div class="col-12 col-md-8 border border-dark  p-5  my-4 mx-auto" id="formPass">
+        <div class="col-12 col-md-8 border border-dark bg-primary bg-gradient p-5  my-4 mx-auto" id="formPass">
             <h3>INGRESA TU NUEVA CONTRASEÑA</h3>
             <form action="" method="POST">
+                <?php if (isset($errores)) : ?>
+                    <?php if (count($errores) != 0) : ?>
+                        <ul class="alert alert-danger mt-3">
+                            <h1>Corregir</h1>
+                            <?php foreach ($errores as  $error) : ?>
+                                <li class="ms-2"><?= $error; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+                <?php endif; ?>
                 <div class="form-group">
                     <label for="usr">Contraseña:</label>
                     <input type="password" class="form-control" id="pass" name="pass" placeholder="Ingresa tu contraseña">
@@ -100,14 +144,13 @@ switch ($error = 'SinError') {
                     <label for="usr2">Repite tu contraseña:</label>
                     <input type="password" class="form-control" id="pass2" name="pass2" placeholder="Repite tu contraseña">
                 </div>
-                <button type="submit" class="btn btn-primary my-3">Cambiar contraseña</button>
+                <button type="submit" class="btn btn-success my-3" name="cambiar_pass">Cambiar contraseña</button>
                 <button type="reset" class="btn btn-warning my-3 mx-3" id="btn-cleanFormPass">Limpiar</button>
             </form>
         </div>
-
     <?php endif; ?>
 <?php else : ?>
-    
+
     <div class="alert alert-danger" role="alert">
         <?php echo $error; ?>
     </div>
