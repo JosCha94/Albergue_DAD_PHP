24780b68882d698c03d46e1d11d34ed2c80dad7a
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2022-06-21 19:34:45 -0500

    perfil usuario

--- BL/consultas_usuario.php
+++ BL/consultas_usuario.php
@@ -211,5 +211,26 @@ class Consulta_usuario
         }
         return $estado;
     }
+
+    public function select_pedidos($conexion, $uid)
+    {
+        try {
+            $sql = "CALL SP_select_pedidos_id($uid)";
+            $consulta = $conexion->prepare($sql);
+            $consulta->execute();
+            $pedido = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $pedido;
+
+        } catch (PDOException $e) {
+            // echo "Ocurrió un ERROR con la base de datos: " .    $e->getMessage();
+            ?>
+              <div class="alert alert-danger alert-dismissible fade show " role="alert">
+              <strong>Error!</strong> Devido a un error en la base de datos, no se pudo mostrar sus pedidos
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+            <?php
+        }        
+    }
 }
 ?>
--- Presentacion/vistas/perfil-usuario.php
+++ Presentacion/vistas/perfil-usuario.php
@@ -17,26 +17,28 @@ switch ($error = 'SinError') {
     $consulta = new Consulta_usuario();
     $id = $_SESSION['usuario'][0];
     $usuario = $consulta->detalleUsuario($conexion, $id);
+    $pedidos = $consulta->select_pedidos($conexion, $id);
     ?>
     <div class="row my-md-4">
         <h1 class="text-center my-4">HOLA <?php echo ($usuario['usuario']); ?></h1>
-        <div class="col-12 col-md-4 bg-warning p-3 position-relative">
+        <div class="col-12 col-md-4 bg-primary p-3 position-relative">
             <h3 class="text-center">Datos del usuario</h3>
 
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col-auto d-none d-lg-block">
                     <img class="img-circle mx-5 mt-3" src="Presentacion\libs\images\perrito_adopt.jpg" width="250" height="250" alt="">
-                    <h3 class="mb-0 text-center mt-2"><?php echo ($usuario['usuario']); ?></h3>
+                    <h3 class="mb-0 text-center  mt-2"><?php echo ($usuario['usuario']); ?></h3>
                 </div>
                 <div class="col p-4 mt-2 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-success">Nombre: <?php echo ($usuario['usr_nombre']); ?></strong>
-                    <div class="mb-1 text-muted">Apellidos: <?php echo ($usuario['usr_apellido_paterno'] . '' . $usuario['usr_apellido_materno']); ?></div>
-                    <div class="mb-1 text-muted">Celular: <?php echo ($usuario['usr_celular']); ?></div>
-                    <div class="mb-1 text-muted">E-mail: <?php echo ($usuario['usr_email']); ?></div>
+                    <!-- <strong class="d-inline-block mb-2 text-white">Nombre: <?php echo ($usuario['usr_nombre']); ?></strong> -->
+                    <div class="mb-1 text-white">Nombre: <?php echo ($usuario['usr_nombre']); ?></div>
+                    <div class="mb-1 text-white">Apellidos: <?php echo ($usuario['usr_apellido_paterno'] . ' ' . $usuario['usr_apellido_materno']); ?></div>
+                    <div class="mb-1 text-white">Celular: <?php echo ($usuario['usr_celular']); ?></div>
+                    <div class="mb-1 text-white">E-mail: <?php echo ($usuario['usr_email']); ?></div>
                 </div>
                 <div class="my-3">
-                    <a class="btn btn-primary w-100 my-4 my-md-2 mx-auto" href="index.php?modulo=update-user&formTipo=dataUser" id="btn-changedata">Cambiar datos</a>
-                    <a class="btn btn-danger w-100 my-2 mx-auto" href="index.php?modulo=update-user&formTipo=passUser" id="btn-changedata">Cambiar contraseña</a>
+                    <a class="btn btn-success w-100 my-4 my-md-2 mx-auto" href="index.php?modulo=update-user&formTipo=dataUser" id="btn-changedata">Cambiar datos</a>
+                    <a class="btn btn-success w-100 my-2 mx-auto" href="index.php?modulo=update-user&formTipo=passUser" id="btn-changedata">Cambiar contraseña</a>
                 </div>
 
             </div>
@@ -47,7 +49,7 @@ switch ($error = 'SinError') {
 
             </div>
         </div>
-        <div class="col-12 col-md-8 bg-primary p-3">
+        <div class="col-12 col-md-8 bg-primary bg-opacity-75 p-3">
             <div class="col-12 bg-danger p-5 my-1">
                 <h3 class="text-center">Adopcion</h3>
             </div>
@@ -56,6 +58,17 @@ switch ($error = 'SinError') {
             </div>
             <div class="col-12 bg-success p-5 my-1">
                 <h3 class="text-center">Mis compras</h3>
+                <ul class="list-group">
+                    <?php foreach ($pedidos as $key => $value) : ?>
+                        <li class="list-group-item"><a href="">Pedido: <?= $value['pedi_id']; ?></a></li>
+                        <!-- <?php echo $value['datos_cliente']; 
+                        $info = json_decode($value['datos_cliente']);
+                        echo $info->cliente;?> 
+                        <?php echo $value['precioCantidad']; 
+                        ?>  -->
+
+                    <?php endforeach; ?>
+                </ul>
             </div>
         </div>
     </div>
