0049716777455a9d1f85090b2bb8035339f92ee4
FZafiro <f.humpiri.pari@isur.edu.pe>
2022-07-01 14:27:38 -0500

    consultas post

---
+++ BL/consultas_post.php
@@ -0,0 +1,29 @@
+<?php
+class Consulta_post
+{
+    public function listarPost($conexion)
+    {
+        try {
+            $sql = "CALL SP_listar_posts()";
+            $consulta = $conexion->prepare($sql);
+            $consulta->execute();
+            $post = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $post;
+        } catch (PDOException $e)  {
+            echo "Ocurrió un ERROR con la base de datos: " .    $e->getMessage();
+        }
+    }
+
+    public function mostrarPost($conexion,$id) {
+        try{
+            $sql = "CALL SP_select_post($id)";
+            $consulta = $conexion->prepare($sql);
+            $consulta->execute();
+            $postId = $consulta->fetch(PDO::FETCH_ASSOC);
+            return $postId;
+        } catch (PDOException $e) {
+            echo "Ocurrió un ERROR con la base de datos: " .    $e->getMessage();
+        }
+    }
+}
+?>
