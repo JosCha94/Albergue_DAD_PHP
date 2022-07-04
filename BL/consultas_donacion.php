a4fa1433f44e59f43057699639a0ef4fe585a9c3
FZafiro <f.humpiri.pari@isur.edu.pe>
2022-07-04 01:28:55 -0500

    actualizar consulta donacion, nombre imagen

--- BL/consultas_donacion.php
+++ BL/consultas_donacion.php
@@ -4,20 +4,21 @@ class Consulta_donacion
     public function insertarDonacion($conexion,$don)
     {
         try {
-            $sql = "CALL SP_insertar_donacion(:nombres, :apellidos, :correo, :celular, :vaucher :tipo_img)";
+            $sql = "CALL SP_insertar_donacion(:nombres, :apellidos, :correo, :celular, :vaucher, :nombre_img :tipo_img)";
             $consulta = $conexion->prepare($sql);
             $consulta->bindValue(':nombres', $don->getDona_nombres());
             $consulta->bindValue(':apellidos', $don->getDona_apellidos());
             $consulta->bindValue(':correo', $don->getDona_correo());
             $consulta->bindValue(':celular', $don->getDona_celular());
             $consulta->bindValue(':vaucher', $don->getDona_vaucher());
+            $consulta->bindValue(':nombre_img', $don->getDona_nombre_img());
             $consulta->bindValue(':tipo_img', $don->getDona_tipo_img());
             $consulta->execute();
         } catch (PDOException $e) {
             echo "Ocurrió un ERROR con la base de datos: " .    $e->getMessage();
         }
     }
-
+    
     public function validar_donacion($don)
     {
         $errores = [];
