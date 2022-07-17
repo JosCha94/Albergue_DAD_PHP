9a5065ea9bb8034eacff1e77f7f15961cb38756c
FabricioRMK3 <fabriciovc16@gmail.com>
2022-07-17 12:18:33 -0500

    perfil usuario corregido

--- Presentacion/vistas/perfil-usuario.php
+++ Presentacion/vistas/perfil-usuario.php
@@ -97,7 +97,9 @@ switch ($error = 'SinError') {
                 </div>
             </div>
         </div>
-        <div class="col-12 col-md-8 p-3 user-adopt">
+            
+
+        <div class="col-12 col-md-8 p-3 user-adopt"> -->
             <div class="accordion" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item one">
                     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
@@ -112,122 +114,88 @@ switch ($error = 'SinError') {
                                     <ul class="borde p-3">
                                         <li class="mx-5 mt-2">Nombre del perrito :<strong><?= $value['perro_nombre'] ?></strong></li>
                                         <li class="mx-5">Fecha de entrevista :<strong><?= $value['adop_fecha_entrevista'] ?></strong></li>
-                                        <li class="mx-5">Estado de la adopción :<strong><?php if ($value['adop_estado'] == 'Rechazada') {
-                                                                                            echo 'Lo sentimos, su solicitud fue rechazada o el perrito fue adoptado por otra persona';
-                                                                                        } else {
-                                                                                            echo $value['adop_estado'];
-                                                                                        } ?></strong></li>
+                                        <li class="mx-5">Estado de la adopción :<strong><?= $value['adop_estado'] ?></strong></li>
                                         <li class="mx-5 mb-2">fecha de adopción :<strong><?= $value['adop_fecha'] ?></strong></li>
                                     </ul>
                                 </div>
                             <?php endforeach; ?>
-                        <?php elseif (count($adop_datos) == 0) : ?>
+                        <?php else : ?>
                             <div class="empty-msg">
                                 Este apartado está vacio
                             </div>
                         <?php endif; ?>
                     </div>
-                <!-- </div>
-
-                <div class="col-12 col-md-8 p-3 user-adopt"> -->
-                    <div class="accordion" id="accordionPanelsStayOpenExample">
-                        <div class="accordion-item one">
-                            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                    Mis Perritos adoptados
-                                </button>
-                            </h2>
-                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                                <?php if (count($adop_datos) > 0) : ?>
-                                    <?php foreach ($adop_datos as $key => $value) : ?>
-                                        <div class="accordion-body">
-                                            <ul class="borde p-3">
-                                                <li class="mx-5 mt-2">Nombre del perrito :<strong><?= $value['perro_nombre'] ?></strong></li>
-                                                <li class="mx-5">Fecha de entrevista :<strong><?= $value['adop_fecha_entrevista'] ?></strong></li>
-                                                <li class="mx-5">Estado de la adopción :<strong><?= $value['adop_estado'] ?></strong></li>
-                                                <li class="mx-5 mb-2">fecha de adopción :<strong><?= $value['adop_fecha'] ?></strong></li>
+                </div>
+                <div class="accordion-item two">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            Mis suscripciones
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                        <?php if (count($sus_datos) > 0) : ?>
+                            <?php foreach ($sus_datos as $key => $value) : ?>
+                                <div class="accordion-body">
+                                    <div class="row borde">
+                                        <div class="col-md-6">
+                                            <ul class="p-3">
+                                                <li class="mx-5 mt-2">Tipo de suscripción : <strong><?= $value['s_tipo_nombre'] ?></strong></li>
+                                                <li class="mx-5">Precio :<strong><?= $value['s_tipo_precio'] ?></strong></li>
+                                                <?php if ($value['suscrip_estado'] == 'Cancelada') : ?>
+                                                    <li class="mx-5 ">Estado de la suscripción: <strong class="text-danger"><?= $value['suscrip_estado'] ?></strong></li>
+                                                <?php else : ?>
+                                                    <li class="mx-5 ">Estado de la suscripción: <strong class="text-success"><?= $value['suscrip_estado'] ?></strong></li>
+                                                <?php endif; ?>
+                                                <li class="mx-5">Fecha de Inicio :<strong><?= $value['suscrip_fecha_inicio'] ?></strong></li>
+                                                <li class="mx-5 mb-2">Fecha de caducidad : <strong><?= $value['suscrip_fecha_termino']; ?></strong></li>
                                             </ul>
                                         </div>
-                                    <?php endforeach; ?>
-                                <?php else : ?>
-                                    <div class="empty-msg">
-                                        Este apartado está vacio
-                                    </div>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                        <div class="accordion-item two">
-                            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                                    Mis suscripciones
-                                </button>
-                            </h2>
-                            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                                <?php if (count($sus_datos) > 0) : ?>
-                                    <?php foreach ($sus_datos as $key => $value) : ?>
-                                        <div class="accordion-body">
-                                            <div class="row borde">
-                                                <div class="col-md-6">
-                                                    <ul class="p-3">
-                                                        <li class="mx-5 mt-2">Tipo de suscripción : <strong><?= $value['s_tipo_nombre'] ?></strong></li>
-                                                        <li class="mx-5">Precio :<strong><?= $value['s_tipo_precio'] ?></strong></li>
-                                                        <?php if ($value['suscrip_estado'] == 'Cancelada') : ?>
-                                                            <li class="mx-5 ">Estado de la suscripción: <strong class="text-danger"><?= $value['suscrip_estado'] ?></strong></li>
-                                                        <?php else : ?>
-                                                            <li class="mx-5 ">Estado de la suscripción: <strong class="text-success"><?= $value['suscrip_estado'] ?></strong></li>
-                                                        <?php endif; ?>
-                                                        <li class="mx-5">Fecha de Inicio :<strong><?= $value['suscrip_fecha_inicio'] ?></strong></li>
-                                                        <li class="mx-5 mb-2">Fecha de caducidad : <strong><?= $value['suscrip_fecha_termino']; ?></strong></li>
-                                                    </ul>
-                                                </div>
-                                                <?php if ($value['suscrip_estado'] == 'Cancelada') : ?>
-                                                    <div class="col-md-6 d-flex align-items-center justify-content-center">
-                                                        <a href="index.php?modulo=apadrinar" class="btn text-white usr-cancel-sus btn-success p-3 mb-3" name="usr-cancel-sus">¡Suscribete denuevo!</a>
-                                                    </div>
-                                                <?php else : ?>
-                                                    <div class="col-md-6 d-flex align-items-center justify-content-center">
-                                                        <form action="" method="POST">
-                                                            <button class="btn usr-cancel-sus btn-warning p-3 mb-3" name="usr-cancel-sus" onclick="return checkDelete()">Cancelar suscripción</button>
-                                                            <input type="hidden" name="cancel-sus" value="<?= $value['suscrip_id']; ?>">
-                                                        </form>
-                                                    </div>
-                                                <?php endif; ?>
-                                                </row>
+                                        <?php if ($value['suscrip_estado'] == 'Cancelada') : ?>
+                                            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                                                <a href="index.php?modulo=apadrinar" class="btn text-white usr-cancel-sus btn-success p-3 mb-3" name="usr-cancel-sus">¡Suscribete denuevo!</a>
                                             </div>
-                                        <?php endforeach; ?>
-                                    <?php else : ?>
-                                        <div class="empty-msg m-3">
-                                            No tienes suscripciones activas en este momento
-                                        </div>
-                                    <?php endif; ?>
-                                        </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item three">
-                            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                                    Mis compras
-                                </button>
-                            </h2>
-                            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse <?php echo (count($pedidos) > 0) ? 'show' : '' ?>" aria-labelledby="panelsStayOpen-headingThree">
-                                <div class="accordion-body">
-                                    <?php if (count($pedidos) > 0) : ?>
-                                        <?php foreach ($pedidos as $key => $value) : ?>
-                                            <form action="index.php?modulo=voucher" method="post">
-                                                <input type="hidden" name="idPedido" value="<?= $value['pedi_id']; ?>">
-                                                <button name="verVoucher" class="btn btn-light w-100 d-flex justify-content-between my-2">
-                                                    <h5>Pedido: <?= $value['pedi_id']; ?></h5> <span class="h5 <?php echo ($value['pedi_estado'] == 'Recogido') ? 'text-success' : 'text-danger' ?>"><?= $value['pedi_estado']; ?></span>
-                                                </button>
-                                            </form>
-                                        <?php endforeach; ?>
-                                    <?php else : ?>
-                                        <div class="empty-msg">
-                                            No tiene compras realizadas en los ultimos 30 dias
-                                        </div>
-                                    <?php endif; ?>
-
+                                        <?php else : ?>
+                                            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                                                <form action="" method="POST">
+                                                    <button class="btn usr-cancel-sus btn-warning p-3 mb-3" name="usr-cancel-sus" onclick="return checkDelete()">Cancelar suscripción</button>
+                                                    <input type="hidden" name="cancel-sus" value="<?= $value['suscrip_id']; ?>">
+                                                </form>
+                                            </div>
+                                        <?php endif; ?>
+                                        </row>
+                                    </div>
+                                <?php endforeach; ?>
+                            <?php else : ?>
+                                <div class="empty-msg m-3">
+                                    No tienes suscripciones activas en este momento
                                 </div>
-                            </div>
+                            <?php endif; ?>
+                                </div>
+                    </div>
+                </div>
+                <div class="accordion-item three">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            Mis compras
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse <?php echo (count($pedidos) > 0) ? 'show' : '' ?>" aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <?php if (count($pedidos) > 0) : ?>
+                                <?php foreach ($pedidos as $key => $value) : ?>
+                                    <form action="index.php?modulo=voucher" method="post">
+                                        <input type="hidden" name="idPedido" value="<?= $value['pedi_id']; ?>">
+                                        <button name="verVoucher" class="btn btn-light w-100 d-flex justify-content-between my-2">
+                                            <h5>Pedido: <?= $value['pedi_id']; ?></h5> <span class="h5 <?php echo ($value['pedi_estado'] == 'Recogido') ? 'text-success' : 'text-danger' ?>"><?= $value['pedi_estado']; ?></span>
+                                        </button>
+                                    </form>
+                                <?php endforeach; ?>
+                            <?php else : ?>
+                                <div class="empty-msg">
+                                    No tiene compras realizadas en los ultimos 30 dias
+                                </div>
+                            <?php endif; ?>
+
                         </div>
                     </div>
                 </div>
